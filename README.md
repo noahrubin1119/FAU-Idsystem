3adab9e710cf197e38da73142992249c8ab0930a
noahrubin1119 <35118067+noahrubin1119@users.noreply.github.com>
2019-07-10 17:26:51 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+FAU ID System
+
+- reads barcodes on student ids to check them into an online google sheet
+
+Noah Rubin and Mr. Perry Weinthal
---
+++ idSystem4.py
@@ -0,0 +1,55 @@
+from urllib.request import urlopen
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import datetime
+import imutils
+import time
+import cv2
+
+
+print("starting video")
+
+vs = VideoStream().start()
+time.sleep(2.0)
+
+
+while True:
+	frame = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	barcodes = pyzbar.decode(frame)
+	# loop over the detected barcodes
+	for barcode in barcodes:
+		# extract the bounding box location of the barcode and draw
+		# the bounding box surrounding the barcode on the image
+		(x, y, w, h) = barcode.rect
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+		# the barcode data is a bytes object so if we want to draw it
+		# on our output image we need to convert it to a string first
+		barcodeData = barcode.data.decode("utf-8")
+		barcodeType = barcode.type
+
+		# draw the barcode data and barcode type on the image
+		text = "{} ({})".format(barcodeData, barcodeType)
+		cv2.putText(frame, text, (x, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		questionMark = len(barcodeData) - 1
+
+		barcodeData = barcodeData[1:questionMark]
+		url = "https://script.google.com/macros/s/AKfycbxFjSuWk3HR-m_b2GOBr7o5M0UlgzJGnUwOWrhDHkGYD6Wr0T-k/exec?tag=" + barcodeData + '&enter=2';
+
+		print(url) 
+		urlopen(url)
+		
+	# show the output frame
+	cv2.imshow("Barcode Scanner", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+ 
+
+cv2.destroyAllWindows()
+vs.stop()
